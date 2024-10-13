9da14c28d2a743699e45575f0c299f52dd0c220b
Abdallah <abdullah2004ash@gmail.com>
2024-10-13 19:03:51 +0300

    Your message

---
+++ source/repos/abd1/abd1/abd1.cpp
@@ -0,0 +1,11 @@
+// abd1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "I'm BARCELONY!\n";
+}
+
+
