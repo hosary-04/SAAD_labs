f2da07f2c3f302679ae0f6194b49c8650bb7128c
Your NameAbdullahgit config --global user.name Your <abdullah2004ash@gmail.com>
2024-12-22 03:03:00 +0200

    delete first two rows

--- source/branch & merge/abd.cpp
+++ source/branch & merge/abd.cpp
@@ -8,8 +8,7 @@ int  sum(int a, int b, int c);
 
 int main()
 {
-    std::cout << "wale\n";
-    std::cout << "lame\n";
+   
     std::cout << "dear student!\n";
     std::cout << "lets try new concepts!\n";
     std::cout << "learning git is fun\n";
